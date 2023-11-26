5c85a4f86b0a753ef886a69c1e03af1ce4bb0c76
acesulfamK <sweetc4h4kno4s@gmail.com>
2023-11-26 10:05:51 +0900

    complete prf.ml

--- prf.ml
+++ prf.ml
@@ -1,4 +1,3 @@
-(* zero関数: 0を返す *)
 let zero arg = 
         match arg with 
         | -1::[] -> -1
@@ -6,7 +5,6 @@ let zero arg =
         | _ -> let _ = print_endline "error: an arg of zero is wrong" in
                 -1
 
-(* suc関数: 後者関数、引数に1を加える *)
 let suc arg = 
         match arg with 
         | -1::[] -> -1
@@ -14,7 +12,6 @@ let suc arg =
         | _ -> let _ = print_endline "error: an arg of suc is wrong" in
                 -1
 
-(* proj関数: 射影関数、n番目の引数を返す。ここでは0オリジンとします *)
 let proj n args = 
         match args with
         | -1::[] -> -1
@@ -29,20 +26,54 @@ type exp =
         | Z
         | S
         | P of (int)
-        | Appl of ((int list -> int) * ((int list -> int) list))
-        | Pr of ((int list -> int) * (int list -> int))
+        | Comp of (exp list)
+        | Pr of (exp * exp)
 
-let trans_exp exp =
-        match exp with
+let rec trans_exp st =
+        match st with
         | Z -> zero
         | S -> suc
         | P(n) -> proj n
-        | Appl(e, li) -> 
+        | Comp(li) -> 
+                let e::es = li in
                 let f = trans_exp e in
-                let li = trans_exp li in (*編集中*)
+                let gs = List.map trans_exp es in
+                let rec function_prod fs x = 
+                          match fs with
+                          | [] -> []
+                          | ff :: ffs -> 
+                              let result = ff x in 
+                              result :: (function_prod ffs x)  
+                          | _ -> 
+                              let _ = print_endline "error: length of function prod args is not equal" in
+                              [-1]  (* エラー処理 *)
+                in
+                fun x -> f (function_prod gs x)
+        | Pr(e1, e2) ->
+                let f = trans_exp e1 in
+                let g = trans_exp e2 in
+                let rec h args =
+                        match args with
+                        | 0::xs -> f xs
+                        | n::xs -> 
+                                if n > 0 then 
+                                        g ((h ((n-1)::xs))::([n-1]@xs))
+                                else 
+                                        let _ = print_endline "error: arg n in Pr is negative" in
+                                                -1
+                        | _ -> let _ = print_endline "error: args of Pr are wrong" in
+                                -1
+                in
+                fun args -> h args
+
+                
         
                 
 (*テストケース*)
-let test1 = trans_exp Z
-let test2 = trans_exp S
-let test3 = trans_exp (P 1)
+let test1 = Z (* zero function*)
+let test2 = S (* suc function *)
+let test3 = P 1 (* projection func to the 2nd var *)
+let test4 = Comp [S; S] (* plus 2 *)
+let test6 = P 0 (* id func *)
+let test7 = Pr (P 0, Comp [S; P 0]) (* add *)
+let add = trans_exp test7
